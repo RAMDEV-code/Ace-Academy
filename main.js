839c9386d42652ecdab719d378f1a688f3a25f31
RAMDEV-code <abdulazeezabdulrahmon1@gmail.com>
2025-07-16 11:15:39 +0100

    ace

--- main.js
+++ main.js
@@ -3,3 +3,13 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('windows-scroll', window.scrollY > 0)
 })
+
+//show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex"
+})
